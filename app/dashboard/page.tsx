7f13a07a9b18045cb32c5fa4ce280741e5a3068d
Anthony Flaherty <aflaherty@certinia.com>
2025-12-12 16:16:45 +0000

    Use Promise.All for data loading

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,14 +9,20 @@ import {
 } from "@/app/lib/data";
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
-  const {
-    numberOfCustomers,
-    numberOfInvoices,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
+  const [
+    revenue,
+    latestInvoices,
+    {
+      numberOfCustomers,
+      numberOfInvoices,
+      totalPaidInvoices,
+      totalPendingInvoices,
+    },
+  ] = await Promise.all([
+    fetchRevenue(),
+    fetchLatestInvoices(),
+    fetchCardData(),
+  ]);
 
   return (
     <main>
