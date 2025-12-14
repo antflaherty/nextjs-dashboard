0c405876d255bb811affc427736e37a226fce06b
Anthony Flaherty <aflaherty@certinia.com>
2025-12-14 19:02:12 +0000

    Create rawFormData object

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,11 @@
 "use server";
 
 export async function createInvoice(formData: FormData) {
-  console.log("server action");
-  console.log(JSON.stringify(formData));
+  const rawFormData = {
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  };
+
+  console.log(rawFormData);
 }
