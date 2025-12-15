c9b55868b8aeb7c9bc2673d26b7bda3ae2448c78
Anthony Flaherty <aflaherty@certinia.com>
2025-12-15 11:40:55 +0000

    Revalidate invoices route after inserting

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 import postgres from "postgres";
 
 import { z } from "zod";
@@ -29,4 +31,6 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+
+  revalidatePath("/dashboard/invoices");
 }
